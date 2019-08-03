34ddc6bde1a22ec4dd10d446b1ef920da2cc9d61
RamalSatharasinghe <ramallakshan@gmail.com>
2019-08-03 10:26:52 +0530

    Complete Channelling(Ramal)

---
+++ app/src/main/java/com/abc/medidoc/fragment_signin.java
@@ -0,0 +1,27 @@
+package com.abc.medidoc;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class fragment_signin extends AppCompatActivity {
+
+    Button btn1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_signin);
+
+        Button btn1 = findViewById(R.id.button2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(fragment_signin.this,channel_details.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
