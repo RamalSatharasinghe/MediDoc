df2a6b0eacb0359c23ca290b283402a35cab3031
kalhanranasinghe <it17418966@my.sliit.lk>
2019-08-03 09:58:30 +0530

    complete_Doc_list(kalhan)

--- app/src/main/java/com/abc/medidoc/MainActivity.java
+++ app/src/main/java/com/abc/medidoc/MainActivity.java
@@ -1,13 +1,16 @@
 package com.abc.medidoc;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
 
@@ -20,11 +23,33 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open,R.string.navigation_drawer_close );
 
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        if(savedInstanceState == null){
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new DiseasesList()).commit();
+        navigationView.setCheckedItem(R.id.nav_list);}
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_list:
+               getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                       new DiseasesList()).commit();
+            break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
